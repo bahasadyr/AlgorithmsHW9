ba612d0054b56cf62e2edc69ea9aa4ab84860323
Baha Sadyr <baha.sadyr777@gmail.com>
2022-05-07 20:23:51 -0500

    added 19 task to main

--- src/main/java/HW10/StringManipulation.java
+++ src/main/java/HW10/StringManipulation.java
@@ -87,7 +87,9 @@ public class StringManipulation {
         }
         return "";
     }
-
+    public String nineteen(String str){
+        return "";
+    }
     public static void main(String[] args) {
         StringManipulation sObj = new StringManipulation();
         String str = "As of March 2022, Java 18 is the latest version, while Java 17, 11 and 8 are the current long-term support (LTS) versions. Oracle released the last zero-cost public update for the legacy version Java 8 LTS in January 2019 for commercial java use, although it will otherwise still support Java 8 with public updates for personal use indefinitely. Other vendors have begun to offer zero-cost builds of OpenJDK 8 and 11 that are still receiving security and other upgrades.";
